c13a3c01630f8646bd33ab6f5919886a4120e71e
ahmedX999 <aaittaleb.354@gmail.com>
2024-01-07 23:40:03 +0100

    second change

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello from 8th version !!:)";
+        return "Hello from 9th version !!:)";
     }
 
     @GetMapping("/user")
